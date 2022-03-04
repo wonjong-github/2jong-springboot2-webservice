13278588a43eb85d314e4ee1c86f491fb8c6f44e
wonjong <david8318@naver.com>
2022-03-05 02:08:06 +0900

    make HelloController test HelloController

---
+++ src/main/java/com/jong/book/jongspringboot2webservice/web/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.jong.book.jongspringboot2webservice.web.controller;
+
+import com.jong.book.jongspringboot2webservice.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
---
+++ src/main/java/com/jong/book/jongspringboot2webservice/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.jong.book.jongspringboot2webservice.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
